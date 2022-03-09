ede50f76a267c33833473268222dfc56ba348da4
Bobby de Vos <bobby_devos@sil.org>
2022-03-09 07:01:17 -0700

    Update source of Gentium import
    The new Gentium sources are updated to the latest upstream versions,
    plus they are built in a more modern, non-RIBBI style of naming the
    weights. This change will enable changing the weight of imported
    Gentium glyphs.

--- addchars.py
+++ addchars.py
@@ -7,18 +7,22 @@ for f in faces:
     workshop = 1.4
     roman = 0.9
     if f == 'ThiruValluvar':
-        upm2048 = 1.0
-        upm1000 = 2048.0/1000.0
+        upm = 1.0
     else:
-        upm2048 = 1000.0/2048.0
-        upm1000 = 1.0
-    scale2048 = str(upm2048*roman)
-    scale1000 = str(upm1000*roman)
+        upm = 1000.0/2048.0
+    scale = str(roman*upm)
 
+    styles = {}
     if f == 'Auvaiyar':
         latin = 'charis'
     else:
         latin = 'gentium'
+        styles = {
+            'Regular': 'Medium',
+            'Italic': 'Medium Italic',
+            'Bold': 'ExtraBold',
+            'Bold Italic': 'ExtraBold Italic'
+        }
 
     for sn in stylesName:
-        modifyFile(scale2048, latin, f, sn, chars = 'latn_import.txt')
+        modifyFile(scale, latin, f, sn, styles, chars='latn_import.txt')
